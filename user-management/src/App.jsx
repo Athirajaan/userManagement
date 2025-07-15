f31fa0b04d4399802a405fed98bb372a93e38b09
Athirajaan <112896823+Athirajaan@users.noreply.github.com>
2025-07-16 02:13:39 +0530

    adds routes and create pages folders

--- user-management/src/App.jsx
+++ user-management/src/App.jsx
@@ -1,8 +1,29 @@
-import React from 'react'
+import React from "react";
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Signin from "./pages/auth/Signin"
+import Signup from "./pages/auth/Signup"
+import Home from "./pages/user/Home"
+import Profile from "./pages/user/Profile";
+import Dashboard from "./pages/admin/Dashboard";
+import AdminLogin from "./pages/admin/AdminLogin";
+
 
 const App = () => {
   return (
-    <div>App</div>
+    <BrowserRouter>
+     <Routes>
+      {/* user route */}
+      <Route path='/login' element={<Signin/>}/>
+      <Route path='/signup' element={<Signup/>}/>
+      <Route path='/' element={<Home/>}/>
+      <Route path='/profile' element={<Profile/>}/>
+      {/* admin routes */}
+      <Route path='/admin/login' element={<AdminLogin/>}/>
+      <Route path='/admin/dashboard' element={<Dashboard/>}/>
+
+      <Route path="*" element={<div>404 - Page Not Found</div>} />
+     </Routes>
+    </BrowserRouter>
   )
 }
 
